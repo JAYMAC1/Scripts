7e50b17c219af76d001e8b59c4c2acbbd960ea0b
jamesmcneil <mcneil_james@hotmail.com>
2017-03-20 21:18:56 +0000

    New Script to set SMB permissions

---
+++ Set-SMBSharePermissions.ps1
@@ -0,0 +1,23 @@
+﻿Function Set-SMBSharePermissions {
+
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [String[]]$ComputerName,
+
+        [Parameter()]
+        [String]$ShareName,
+
+        [Parameter()]
+        [String]$Trustee,
+
+        [Parameter()]
+        [String]$AccessMask
+    )
+
+    BEGIN{}
+    PROCESS{}
+    END{}
+
+
+}

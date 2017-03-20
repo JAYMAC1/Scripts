1f60ca34aa5dc8689d1f2648e234865dc9d29218
jamesmcneil <mcneil_james@hotmail.com>
2017-03-20 20:35:26 +0000

    Nothing new added

---
+++ Get-SMBSharePermissions.ps1
@@ -0,0 +1,33 @@
+﻿Function Get-SMBSharePermissions {
+
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [String[]]$ComputerName,
+
+        [Parameter()]
+        [Switch]$Log,
+
+        [Parameter()]
+        [String]$LogPath
+    )
+
+    BEGIN{}
+    PROCESS{
+        ForEach($Computer in $ComputerName){
+            $continue = $false
+            Try {
+                Write-Verbose("Trying to connect to {0}" -f $Computer)
+                $shares = Get-WmiObject -Class Win32_LogicalShareSecuritySetting -ErrorAction Stop
+                $continue = $true
+            } Catch {
+                
+            }
+        }
+    
+    }
+    END{}
+
+}
+
+$shares = Get-WmiObject -Class Win32_LogicalShareSecuritySetting
